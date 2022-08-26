272c0e93fc3c25c57b96f48d88f49f3304538bb0
Santiago Acosta <acostapuertasantiago@gmail.com>
2022-08-26 14:01:26 -0500

    primer commit

---
+++ ejercicio_git.py
@@ -0,0 +1,6 @@
+from gettext import npgettext
+from multiprocessing.spawn import import_main_path
+import pandas as pd
+import numpy as np
+
+print('Somos el equipo de analitica mercadeo cueros Vélez')
