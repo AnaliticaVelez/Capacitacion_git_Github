6416cb4123bc4588e339ee7747347161f7120e60
Santiago Acosta <acostapuertasantiago@gmail.com>
2022-08-26 14:37:03 -0500

    Mejore ortografia y nombre del desarrollador

--- ejercicio_git.py
+++ ejercicio_git.py
@@ -3,13 +3,15 @@ from multiprocessing.spawn import import_main_path
 import pandas as pd
 import numpy as np
 
-print('Somos el equipo de analitica mercadeo cueros Vélez')
+print('Somos el equipo de analtica mercadeo Cueros Vélez')
 
 
 def rombo(n):
 
-    result1=[" "*(n-i)+"*"*(i+i-1) for i in range(1,n+1)]
+    result1=[" "*(n-i)+"$"*(i+i-1) for i in range(1,n+1)]
     return "\n".join(result1+list(reversed(result1[:-1])))
  
 numero=int(input("indica un numero: "))
-print(rombo(numero))
+print(rombo(numero))´
+
+print("Desarollado por Simon")
